f383407186e1b77422e77164baea9e7ad6d24188
AdasKaminski <50117773+AdasKaminski@users.noreply.github.com>
2020-01-11 21:19:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wypov1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            licence licence = new licence();
+            licence.DisplayLicence();
+        }
+       
+    }
+}
---
+++ licence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace wypov1
+{
+    class licence
+    {
+        public static void DisplayLicence()
+        {
+            int day = 15;
+            bool t = true;
+            while (t)
+            {
+                Console.WriteLine("Wybierz jaką opcję chcesz wybrać: ");
+
+                Console.WriteLine("[1] Przedłóż licencję");
+                Console.WriteLine("[2] Ile dni pozostało do końca licencji");
+
+                Console.WriteLine("[0] Wyjście");
+
+                int i;
+                int.TryParse(Console.ReadLine(), out i);
+
+
+
+                if (i == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine(day + 365);
+                    Console.WriteLine("Licencja została przedłóżona");
+                    Console.ReadKey();
+                    t = false;
+                }
+               else if (i == 2)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Do końca licencji zostało "+day);
+                    Console.ReadKey();
+                    t = false;
+                }
+               else if (i == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Koniec");
+                    t = false;
+                }
+            }
+        }
+    }
+}
