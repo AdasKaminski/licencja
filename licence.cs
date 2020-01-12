1372c25a072a40595ba4f8a52e659f182ab52fc3
AdasKaminski <50117773+AdasKaminski@users.noreply.github.com>
2020-01-12 14:08:28 +0100

    Add files via upload

--- licence.cs
+++ licence.cs
@@ -6,14 +6,18 @@ namespace wypov1
     {
         public static void DisplayLicence()
         {
-            int day = 15;
+            DateTime date = DateTime.Now.Date;
+            DateTime date1 = DateTime.Parse("17-01-2020");
+           
             bool t = true;
             while (t)
             {
                 Console.WriteLine("Wybierz jaką opcję chcesz wybrać: ");
-
+                Console.WriteLine();
+                Console.WriteLine("Do końca twojej licencji zostało:");
+                Console.WriteLine(date1-date);
                 Console.WriteLine("[1] Przedłóż licencję");
-                Console.WriteLine("[2] Ile dni pozostało do końca licencji");
+                
 
                 Console.WriteLine("[0] Wyjście");
 
@@ -25,19 +29,12 @@ namespace wypov1
                 if (i == 1)
                 {
                     Console.Clear();
-                    Console.WriteLine(day + 365);
-                    Console.WriteLine("Licencja została przedłóżona");
-                    Console.ReadKey();
-                    t = false;
+                   Console.WriteLine(DateTime.Now.AddDays(365));
+                  Console.WriteLine("Licencja została przedłóżona");
+                 Console.ReadKey();
+                   t = false;
                 }
-               else if (i == 2)
-                {
-                    Console.Clear();
-
-                    Console.WriteLine("Do końca licencji zostało "+day);
-                    Console.ReadKey();
-                    t = false;
-                }
+              
                else if (i == 0)
                 {
                     Console.Clear();
